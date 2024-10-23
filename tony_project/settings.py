73c3af618a2adc9f0a73cbc676b492108ffee74c
TONY65391 <tonyanthony65391@gmail.com>
2024-10-23 05:24:42 -0700

    add requirements.txt

--- tony_project/settings.py
+++ tony_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p(4e*+hw-9dek&k%%wjdfp+2_!#kx1v508k-a_l4@^%)0u-k+$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
