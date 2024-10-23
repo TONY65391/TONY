5dff381915ccc9c96663824dbddf5322d13088d2
TONY65391 <tonyanthony65391@gmail.com>
2024-10-23 06:49:21 -0700

    first commit

--- tony_app/urls.py
+++ tony_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('js1/', views.jss1, name = 'js1'),
     path('js1/djs1/<int:id>', views.djss1, name = 'djs1'),
     path('js2/', views.jss2, name = 'js2'),
-    path('js2/djs2/<int:i>', views.djss2, name = 'djs2'),
+    path('js2/djs2/<int:id>', views.djss2, name = 'djs2'),
     path('js3/', views.djss3, name = 'djss3'),
     path('js3/djs3/<int:id>', views.djss3, name = 'djss3' )
 ]
